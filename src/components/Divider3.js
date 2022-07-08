ae50c8d933d86b164da8af45fa45ac675161c3fe
DomenicoCentrone <76876906+DomenicoCentrone@users.noreply.github.com>
2022-07-08 12:07:33 +0200

    .

---
+++ src/components/Divider3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function divider() {
+  return (
+    <div className="divider3"></div>
+  )
+}
+
+export default divider
--- src/components/Jdm.js
+++ src/components/Jdm.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import Divider from "../components/Divider";
+import Divider3 from "../components/Divider3";
 import { Container, Row, Col } from "react-bootstrap";
 
 
 function Jdm() {
   return (
     <section className="boxe" id="jdm">
-        <Divider />
         <Container>
-            <Row>
-                <div className="col-md-12"><p className="dec-gray font-frak">Frog's Featerues</p></div>
+            <Row className="jdm-img">
+                <div className="col-md-12 jdm-margin text-center"><h7>Play video and know more about our dispute system</h7>
+                <h1>JDM & Judge to earn</h1></div>
             </Row>
             <Row>
-                <div className="col-md-6"><h1>Jdm and Protected Payments</h1></div>
-                <div className="col-md-6"><img className="img-fluid" src=".\img\frogsword.png"></img></div>
-            </Row>
-            <Row>
-              <div className="col-md-12">
-                <img className="img-fluid" src=".\img\line-dec.png"></img>
-              </div>
-              <div className="col-md-12">
-                <p className="font-frak">Security & Decentralization toghether for the first time</p>
-              </div>
+                <div className="col-md-6">
+                    <p className="less">Our revolutionary Jury Dispute Management system:</p>
+                    <ol>
+                        <li className="less">Fair & Decentralised</li>
+                        <li className="less">Anonimous votes</li>
+                        <li className="less">Earn rewards by Judge individual <br></br> transaction</li>
+                    </ol>
+                </div>
             </Row>
         </Container>
     </section>
